348c2c31e3b8b03be02cca7c8dc9fb8585ce8bb3
vicoduor <victoroduorr@gmail.com>
2025-01-31 16:55:17 +0300

    .

--- api/views.py
+++ api/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.utils.timezone import now
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET
-
+from datetime import datetime,timezone
 
 @csrf_exempt
 @require_GET
 def hng_public_api(request):
     response_data = {
         "email": "victoroduorr@gmail.com",
-        "current_datetime": now().isoformat(timespec='milliseconds').replace("+00:00", "Z"),
+        "current_datetime": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
         "github_url": "https://github.com/OviLab-sys/hng-stage0"
     }
     return JsonResponse(response_data)
